733c7b65a9ba059ac3824867eb35e892ab4caa49
Hekzagonal-git <sunnythebirbball@gmail.com>
2025-04-03 14:16:40 -0600

    JSON Changes + Planning
    Function organization / structure is planned out, JSON object formatting is finalized, Game development can properly begin now

--- two-dimensional-array-assignment/sketch.js
+++ two-dimensional-array-assignment/sketch.js
@@ -5,6 +5,22 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let tetrominoFigures;
+let tetrominoOffsetData;
+let levelGravities;
+
+class Tetromino {
+  constructor() {
+
+  }
+  
+}
+
+function preload() {
+  tetrominoFigures = loadJSON('/data-tables/tetromino-figures.json');
+  tetrominoOffsetData = loadJSON('/data-tables/offset-data.json');
+  levelGravities = loadJSON('/data-tables/level-gravities.json');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,4 +29,6 @@ function setup() {
 function draw() {
   background(220);
 }
+
+// IJLOSTZ
 
