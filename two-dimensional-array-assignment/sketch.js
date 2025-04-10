a6de2621447926a9d8a7f519bf5c79bb1dc77e67
Hekzagonal-git <sunnythebirbball@gmail.com>
2025-04-09 18:20:29 -0600

    Tetris() Class MAJOR progress
    Big day for this project! I brought over most of the class methods from tetris.py to sketch.js!
    I'm currently still feeling unsure about my "tasks" system as a replacement for pygame's events, but I think it'll prove to be beneficial if I decide to implement multiplayer.

--- two-dimensional-array-assignment/sketch.js
+++ two-dimensional-array-assignment/sketch.js
@@ -1,19 +1,21 @@
-// Project Title
-// Your Name
-// Date
+// 2D Array Assignment
+// Luke Pawle-Fahy
+// 4/9/2025
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - I don't think I've done anything Extra yet, but that will be the case when this is finished as my Final Project.
 
 const MATRIX_WIDTH = 10;
 const MATRIX_HEIGHT = 20;
+const QUEUE_LENGTH = 5;
 
 let tetrominoFigures;
 let tetrominoOffsetData;
 let levelGravities;
 
 let tasks = [];
-// {timer = Timer(), onExpiry, ...parameters};
+// {timer = Timer(), onExpiry = function, ...args};
+// example: {timer = Timer(3000), onExpiry = movePieceLeft, args = idk}
 
 class Tetromino {
   constructor(type = 0) {
@@ -32,12 +34,14 @@ class Tetromino {
 
   }
 
-  image(rotationDifference = 0) {
-    return tetrominoFigures[this.type][(this.rotation + rotationDifference) % 4];
+  image(rotation = this.rotation, type = this.type) {
+    return tetrominoFigures[type][rotation];
   }
+
 }
 
 class Tetris {
+  // Setup
   constructor() {
     // Stats
     this.score = 0,
@@ -46,20 +50,22 @@ class Tetris {
 
     // Tetromino Placement
     this.activePiece = null,
-    this.heldPiece = null,
+    this.heldPieceType = null,
     this.queue = [],
     this.matrix = createNewMatrix(),
     
     // Movement
     this.holdUsed = false,
-    this.lockDelay = false,
     this.droppingHard = false,
     this.droppingSoft = false,
-
-    this.delayedAutoStart = false,
+    this.delayedAutoStart = null,
     this.autoRepeat = true,
+    
+    // Lock Delay & Move Reset
+    this.lockDelay = false,
+    this.moveResetCounter = 0,
 
-    // 
+    // Game State
     this.active = true;
   }
 
@@ -72,6 +78,169 @@ class Tetris {
       }
     }
     return newMatrix;
+  }
+
+  // Active Piece Methods
+  rotateTetromino(rotationDistance = 0) {
+    if (this.activePiece !== null) {
+      let newRotation = (this.activePiece.rotation + rotationDistance) % 4;
+      switch (this.activePiece.type) {
+        case 0:
+          for (let i = 0; i < tetrominoOffsetData[1].table[0].length; i++) {
+            let offsetX = tetrominoOffsetData[1].table[this.activePiece.rotation][i][0] - tetrominoOffsetData[1].table[newRotation][i][0];
+            let offsetY = tetrominoOffsetData[1].table[this.activePiece.rotation][i][1] - tetrominoOffsetData[1].table[newRotation][i][1];
+            if (!this.intersects(this.activePiece.x + offsetX, this.activePiece.y - offsetY, newRotation, undefined)) {
+              this.activePiece.x += offsetX;
+              this.activePiece.y -= offsetY;
+              this.activePiece.rotation = newRotation
+              break;
+            }
+          }
+          break;
+        case 3:
+          for (let i = 0; i < tetrominoOffsetData[2].table[0].length; i++) {
+            let offsetX = tetrominoOffsetData[2].table[this.activePiece.rotation][i][0] - tetrominoOffsetData[2].table[newRotation][i][0];
+            let offsetY = tetrominoOffsetData[2].table[this.activePiece.rotation][i][1] - tetrominoOffsetData[2].table[newRotation][i][1];
+            if (!this.intersects(this.activePiece.x + offsetX, this.activePiece.y - offsetY, newRotation, undefined)) {
+              this.activePiece.x += offsetX;
+              this.activePiece.y -= offsetY;
+              this.activePiece.rotation = newRotation
+              break;
+            }
+          }
+          break;
+        default:
+          for (let i = 0; i < tetrominoOffsetData[0].table[0].length; i++) {
+            let offsetX = tetrominoOffsetData[0].table[this.activePiece.rotation][i][0] - tetrominoOffsetData[0].table[newRotation][i][0];
+            let offsetY = tetrominoOffsetData[0].table[this.activePiece.rotation][i][1] - tetrominoOffsetData[0].table[newRotation][i][1];
+            if (!this.intersects(this.activePiece.x + offsetX, this.activePiece.y - offsetY, newRotation, undefined)) {
+              this.activePiece.x += offsetX;
+              this.activePiece.y -= offsetY;
+              this.activePiece.rotation = newRotation
+              break;
+            }
+          }
+          break;
+      }
+    }
+    else {
+      // Throw an error
+    }
+  }
+
+  moveTetrominoX(dx = 0) {
+    if (this.activePiece !== null && !this.intersects(this.activePiece.x + dx, undefined)) {
+      this.activePiece.x += dx;
+    }
+  }
+
+  moveTetrominoY(dy = 1) {
+    // If this.activePiece exists,
+    if (this.activePiece !== null) {
+
+      // Check if the destination intersects with anything.
+      if (this.intersects(undefined, this.activePiece.y + dy)) {
+
+        // Shift Tetromino back out until it no longer intersects.
+        while (this.intersects(undefined, this.activePiece.y + dy)) {
+          if (dy > 0) {
+            this.activePiece.y -= 1;
+          }
+          else {
+            this.activePiece.y += 1;
+          }
+        }
+
+        if (this.shouldPlaceTetromino()) {
+          this.placeTetromino();
+        }
+
+        // Start the 0.5 second Lock Delay timer if it isn't currently happening.
+        else if (!this.lockDelay) {
+          this.lockDelay = true;
+          this.moveResetCounter = 0;
+          
+        }
+      }
+      else {
+        // idk
+      }
+    }
+  }
+
+  placeTetromino(x = this.activePiece.x, y = this.activePiece.y, rotation = this.activePiece.rotation, type = this.activePiece.type) {
+    if (!this.intersects(x, y, rotation, type) && this.intersects(x, y + 1, rotation, type)) {
+      for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+          if (this.activePiece.image(rotation, type).includes(i * 5 + j)) {
+            this.matrix[y + i][x + j] = this.activePiece.type;
+          }
+        }
+      }
+    }
+  }
+
+  holdTetromino() {
+    if (this.activePiece !== null && !this.holdUsed) {
+      [this.activePiece, this.heldPieceType] = [Tetromino(this.heldPieceType), this.activePiece.type];
+    }
+  }
+
+  // Matrix Methods
+  clearLines() {
+    let lines = 0;
+    for (let i = 0; i < this.matrix.length - 1; i++) {
+      if (this.matrix[i].indexOf(null) === -1) {
+        lines += 1;
+        for (let i1 = i; i1 > 1; i1--) {
+          for (let j = 0; j < this.matrix[i1].length - 1; j++) {
+            this.matrix[i1][j] = this.matrix[i1 - 1][j];
+            // This is exactly what i did in python, but it feels like it shouldn't work???
+          }
+        }
+      }
+    }
+  }
+
+  // Queue Methods
+  advanceNextQueue() {
+    if (this.activePiece === null) {
+      this.activePiece = Tetromino(this.queue.shift());
+    }
+    else {
+      // throw error
+    }
+  }
+
+  refillNextQueue() {
+    if (this.queue.length < QUEUE_LENGTH) {
+      this.queue.push(...shuffle([0, 1, 2, 3, 4, 5, 6, 7]));
+    }
+  }
+
+  // Conditionals
+  shouldPlaceTetromino() {
+    return (this.droppingHard || this.moveResetCounter >= 15);
+  }
+
+  intersects(x = this.activePiece.x, y = this.activePiece.y, rotation = this.activePiece.rotation, type = this.activePiece.type) {
+    let intersection = false;
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        if (this.activePiece.image(rotation, type).includes(i * 5 + j)) {
+          if (
+            this.matrix[y + i][x + j] !== null ||
+            y + i < 0 ||
+            y + i > this.matrix.length - 1 ||
+            x + j < 0 ||
+            x + j > this.matrix[y + i].length - 1
+          ) {
+            intersection = true;
+          }
+        }
+      }
+    }
+    return intersection;
   }
 }
 
@@ -90,10 +259,11 @@ function draw() {
 }
 
 function checkTimers() {
+  // Checks each task, sees if its timer is expired. if so, executes the specified function.
   for (let task of tasks) {
     // In any case it should be onExpiry(..args), right? then the onExpiry function can redirect to whatever object you need. That sounds right.
     if (task.timer.expired()) {
-      // do something
+      task.onExpiry(...task.args)
     }
   }
 }
